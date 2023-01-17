b401d763a4fa764bb989ec235ab8ac5a2bff8c90
karthick <karthi.16v@gmail.com>
2023-01-17 22:50:33 +0530

    new

--- src/files/proj.js
+++ src/files/proj.js
@@ -16,26 +16,26 @@ const Proj = () => {
        <Cards img='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'
         name='NETFLIX-CLONE'
          des='Netflix-clone is a FrontEnd App.It is Create  React,Javascript,Html and Css' 
-         codelink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'
-          deplink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'/>
+         codelink='https://github.com/karthicK007V/Frontend-Netflix'
+          deplink="https://netflixs-clo.netlify.app/"/>
 
 <Cards img='https://cdn.dribbble.com/users/925716/screenshots/4531553/weather_app_background_dribbble_size_prem_gurusamy.gif'
         name='Weather-App'
          des='Netflix-clone is a FrontEnd App.It is Create  React,Javascript,Html and Css' 
-         codelink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'
-          deplink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'/>
+         codelink='https://github.com/karthicK007V/Frontend-weather'
+          deplink='https://rea-weather-app.netlify.app/'/>
 
 <Cards img='https://thumbs.dreamstime.com/b/apple-store-sindelfingen-germany-46310108.jpg'
         name='Iphone Store'
          des='Netflix-clone is a FrontEnd App.It is Create  React,Javascript,Html and Css' 
-         codelink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'
-          deplink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'/>
+         codelink='https://github.com/karthicK007V/Mern-project2-Frontend'
+          deplink='https://iphone-store-clone.netlify.app/'/>
 
 <Cards img='https://media.istockphoto.com/id/922783734/photo/assorted-indian-recipes-food-various.jpg?s=170667a&w=is&k=20&c=fTrebHtSaNR54AmOfW-DJBsPt7EPCLpFHohA9V83d4Y='
         name='Food Order App'
          des='Netflix-clone is a FrontEnd App.It is Create  React,Javascript,Html and Css' 
-         codelink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'
-          deplink='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSxTGsBauUgDpzMWInjobjcyRSS5iP5x0nIyQ&usqp=CAU'/>
+         codelink='"https://github.com/karthicK007V/Mern-project1-Frontend'
+         deplink='https://food-orders-management.netlify.app/'/>
 
 
 
